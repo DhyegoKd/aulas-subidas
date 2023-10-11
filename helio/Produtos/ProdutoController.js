98fef197715e0a79ff5c6287792094733a93d98e
DhyegoKd <dhyegokaderlinedk@gmail.com>
2023-10-11 11:26:41 -0300

    Submit

--- helio/Produtos/ProdutoController.js
+++ helio/Produtos/ProdutoController.js
@@ -1,37 +1,48 @@
 const express = require("express");
 const router = express.Router()
-const Produto = require ("./Produto");
+const Produto = require("./Produto");
 const bodyParser = require("body-parser");
 
-router.use(bodyParser.urlencoded({extended : false}));
+router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
 
 
-router.get("/produto/novo", (req, res)=>{
-     res.render("produtos/novo");
- });
- 
- router.post("/salvarProduto", (req, res)=>{
-     var titulo = req.body.titulo;
-     var descricao = req.body.descricao;
-     Produto.create({
-         titulo : titulo,
-         descricao : descricao
-     }).then (()=>{
-         res.redirect("/produtos");
-     });
- });
- 
- router.get ("/produtos", (req, res)=>{
-     Produto.findAll({raw : true }).then(produtos=>{
-         console.log (produtos);
-         res.render("produtos/index", {
-            produtos : produtos
-         });
-     });
-     
- });
+router.get("/produtos/novo", (req, res) => {
+    res.render("produtos/novo");
+});
 
- module.exports = router;
- 
- 
+router.post("/salvarProduto", (req, res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Produto.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/produtos");
+    });
+});
+
+router.get("/produtos", (req, res) => {
+    Produto.findAll({ raw: true }).then(produtos => {
+        console.log(produtos);
+        res.render("produtos/index", {
+            produtos: produtos
+        });
+    });
+});
+
+router.post("/produtos/delete", (req, res) => {
+    var id = req.body.id;
+
+    Produto.destroy({
+        where: {
+            id : id
+        }
+
+    }).then(()=>{
+        res.redirect("/produtos");
+
+    })
+});
+
+module.exports = router;
